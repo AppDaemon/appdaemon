38787628d0532dd4b5d70166b368da4a3d1cdd2c
QuadPortNick <quadportnick@users.noreply.github.com>
2017-04-01 09:23:31 -0400

    Testing new Docker options
    Startup script that configures with supplied variables
    Easier to pass command lines to appdaemon

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,30 @@
 FROM python:3.4
 
+#For Raspberry Pi use this instead
+#FROM resin/raspberrypi-python:3.4
+
+VOLUME /conf
+VOLUME /certs
+EXPOSE 5050
+
+# Environment vars we can configure against
+# But these are optional, so we won't define them now
+#ENV HA_URL http://hass:8123
+#ENV HA_KEY secret_key
+#ENV DASH_URL http://hass:5050
+#ENV EXTRA_CMD -D DEBUG
+
+# Copy appdaemon into image
 RUN mkdir -p /usr/src/app
 WORKDIR /usr/src/app
-VOLUME /conf
-
-# Copy source
 COPY . .
 
-# INSTALL
+# Docker specific mods
+RUN sed -i 's/loop.create_server(handler, conf.dash_host/loop.create_server(handler, "0.0.0.0"/g' /usr/src/app/appdaemon/appdash.py
 
+# Install
 RUN pip3 install .
 
-CMD [ "appdaemon", "-c", "/conf" ]
+# Start script
+RUN chmod +x /usr/src/app/dockerStart.sh
+CMD [ "./dockerStart.sh" ]
---
+++ dockerStart.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+CONF=/conf
+CONF_SRC=/usr/src/app/conf
+
+# if configuration file doesn't exist, copy the default
+if [ ! -f $CONF/appdaemon.cfg ]; then
+  cp $CONF_SRC/appdaemon.cfg.example $CONF/appdaemon.cfg
+fi
+
+# if apps folder doesn't exist, copy the default
+if [ ! -d $CONF/apps ]; then
+  cp -r $CONF_SRC/apps $CONF/apps
+fi
+
+# if dashboards folder doesn't exist, copy the default
+if [ ! -d $CONF/dashboards ]; then
+  cp -r $CONF_SRC/dashboards $CONF/dashboards
+fi
+
+# if ENV HA_URL is set, change the value in appdaemon.cfg
+if [ -n "$HA_URL" ]; then
+  sed -i "s/^ha_url.*/ha_url = $(echo $HA_URL | sed -e 's/\\/\\\\/g; s/\//\\\//g; s/&/\\\&/g')/" $CONF/appdaemon.cfg
+fi
+
+# if ENV HA_KEY is set, change the value in appdaemon.cfg
+if [ -n "$HA_KEY" ]; then
+  sed -i "s/^ha_key.*/ha_key = $(echo $HA_KEY | sed -e 's/\\/\\\\/g; s/\//\\\//g; s/&/\\\&/g')/" $CONF/appdaemon.cfg
+fi
+
+# if ENV DASH_URL is set, change the value in appdaemon.cfg
+if [ -n "$DASH_URL" ]; then
+  if grep -q "^dash_url" $CONF/appdaemon.cfg; then
+    sed -i "s/^dash_url.*/dash_url = $(echo $DASH_URL | sed -e 's/\\/\\\\/g; s/\//\\\//g; s/&/\\\&/g')/" $CONF/appdaemon.cfg
+  else
+    sed -i "s/\[AppDaemon\]/\[AppDaemon\]\r\ndash_url = $(echo $DASH_URL | sed -e 's/\\/\\\\/g; s/\//\\\//g; s/&/\\\&/g')/" $CONF/appdaemon.cfg
+  fi
+fi
+
+# Lets run it!
+appdaemon -c $CONF $EXTRA_CMD
