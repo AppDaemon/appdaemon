748dab401f75045cdf2db5442ab2dd61ace67aae
Andrew Cockburn <andrew@acockburn.com>
2017-07-08 14:17:17 -0400

    Fix ,cfg vs .yaml

--- dockerStart.sh
+++ dockerStart.sh
@@ -5,7 +5,7 @@ CONF_SRC=/usr/src/app/conf
 
 # if configuration file doesn't exist, copy the default
 if [ ! -f $CONF/appdaemon.yaml ]; then
-  cp $CONF_SRC/appdaemon.yaml.example $CONF/appdaemon.cfg
+  cp $CONF_SRC/appdaemon.yaml.example $CONF/appdaemon.yaml
 fi
 
 # if apps folder doesn't exist, copy the default
@@ -18,22 +18,22 @@ if [ ! -d $CONF/dashboards ]; then
   cp -r $CONF_SRC/dashboards $CONF/dashboards
 fi
 
-# if ENV HA_URL is set, change the value in appdaemon.cfg
+# if ENV HA_URL is set, change the value in appdaemon.yaml
 if [ -n "$HA_URL" ]; then
-  sed -i "s/^  ha_url:.*/ha_url: $(echo $HA_URL | sed -e 's/\\/\\\\/g; s/\//\\\//g; s/&/\\\&/g')/" $CONF/appdaemon.cfg
+  sed -i "s/^  ha_url:.*/ha_url: $(echo $HA_URL | sed -e 's/\\/\\\\/g; s/\//\\\//g; s/&/\\\&/g')/" $CONF/appdaemon.yaml
 fi
 
-# if ENV HA_KEY is set, change the value in appdaemon.cfg
+# if ENV HA_KEY is set, change the value in appdaemon.yaml
 if [ -n "$HA_KEY" ]; then
-  sed -i "s/^  ha_key:.*/ha_key: $(echo $HA_KEY | sed -e 's/\\/\\\\/g; s/\//\\\//g; s/&/\\\&/g')/" $CONF/appdaemon.cfg
+  sed -i "s/^  ha_key:.*/ha_key: $(echo $HA_KEY | sed -e 's/\\/\\\\/g; s/\//\\\//g; s/&/\\\&/g')/" $CONF/appdaemon.yaml
 fi
 
-# if ENV DASH_URL is set, change the value in appdaemon.cfg
+# if ENV DASH_URL is set, change the value in appdaemon.yaml
 if [ -n "$DASH_URL" ]; then
-  if grep -q "^  dash_url" $CONF/appdaemon.cfg; then
-    sed -i "s/^  dash_url:.*/dash_url: $(echo $DASH_URL | sed -e 's/\\/\\\\/g; s/\//\\\//g; s/&/\\\&/g')/" $CONF/appdaemon.cfg
+  if grep -q "^  dash_url" $CONF/appdaemon.yaml; then
+    sed -i "s/^  dash_url:.*/dash_url: $(echo $DASH_URL | sed -e 's/\\/\\\\/g; s/\//\\\//g; s/&/\\\&/g')/" $CONF/appdaemon.yaml
   else
-    sed -i "s/\[AppDaemon\]/\[AppDaemon\]\r\n  dash_url: $(echo $DASH_URL | sed -e 's/\\/\\\\/g; s/\//\\\//g; s/&/\\\&/g')/" $CONF/appdaemon.cfg
+    sed -i "s/\[AppDaemon\]/\[AppDaemon\]\r\n  dash_url: $(echo $DASH_URL | sed -e 's/\\/\\\\/g; s/\//\\\//g; s/&/\\\&/g')/" $CONF/appdaemon.yaml
   fi
 fi
 
