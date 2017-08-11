91c4d14b062c086cca752960734d5dba26a78667
Andrew Cockburn <andrew@acockburn.com>
2017-08-11 07:42:08 -0400

    Add raspbian deps to docs

--- appdaemon/appapi.py
+++ appdaemon/appapi.py
@@ -14,6 +14,51 @@ import appdaemon.homeassistant as ha
 
 reading_messages = False
 
+class Formatter(object):
+    def __init__(self):
+        self.types = {}
+        self.htchar = '\t'
+        self.lfchar = '\n'
+        self.indent = 0
+        self.set_formater(object, self.__class__.format_object)
+        self.set_formater(dict, self.__class__.format_dict)
+        self.set_formater(list, self.__class__.format_list)
+        self.set_formater(tuple, self.__class__.format_tuple)
+
+    def set_formater(self, obj, callback):
+        self.types[obj] = callback
+
+    def __call__(self, value, **args):
+        for key in args:
+            setattr(self, key, args[key])
+        formater = self.types[type(value) if type(value) in self.types else object]
+        return formater(self, value, self.indent)
+
+    def format_object(self, value, indent):
+        return repr(value)
+
+    def format_dict(self, value, indent):
+        items = [
+            self.lfchar + self.htchar * (indent + 1) + repr(key) + ': ' +
+            (self.types[type(value[key]) if type(value[key]) in self.types else object])(self, value[key], indent + 1)
+            for key in value
+        ]
+        return '{%s}' % (','.join(items) + self.lfchar + self.htchar * indent)
+
+    def format_list(self, value, indent):
+        items = [
+            self.lfchar + self.htchar * (indent + 1) + (self.types[type(item) if type(item) in self.types else object])(self, item, indent + 1)
+            for item in value
+        ]
+        return '[%s]' % (','.join(items) + self.lfchar + self.htchar * indent)
+
+    def format_tuple(self, value, indent):
+        items = [
+            self.lfchar + self.htchar * (indent + 1) + (self.types[type(item) if type(item) in self.types else object])(self, item, indent + 1)
+            for item in value
+        ]
+        return '(%s)' % (','.join(items) + self.lfchar + self.htchar * indent)
+
 class AttrDict(dict):
     """ Dictionary subclass whose entries can be accessed by attributes
         (as well as normally).
@@ -164,15 +209,22 @@ class AppDaemon:
         else:
             return None
 
-    def get_alexa_slot_value(self, data, slot):
+    def get_alexa_slot_value(self, data, slot = None):
         if "request" in data and \
                         "intent" in data["request"] and \
-                        "slots" in data["request"]["intent"] and \
-                        slot in data["request"]["intent"]["slots"] and \
+                        "slots" in data["request"]["intent"]:
+            if slot == None:
+                return(data["request"]["intent"]["slots"])
+            else:
+                if slot in data["request"]["intent"]["slots"] and \
                         "value" in data["request"]["intent"]["slots"][slot]:
-            return(data["request"]["intent"]["slots"][slot]["value"])
+                    return(data["request"]["intent"]["slots"][slot]["value"])
+                else:
+                    return None
         else:
             return None
+
+        return response
 
     def format_alexa_response(self, speech = None, card = None, title = None):
 
