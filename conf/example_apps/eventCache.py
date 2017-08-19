684ef48eacf914b7696cdbc360059dd041a96a09
Torkild Retvedt <torkild@retvedt.no>
2017-08-19 12:18:33 +0200

    Add example apps for bysykkel, yr, ical, ruter, eventCache and eventMonitor
    These apps are all a bit more "Hadashboard" centric than the other
    examples. These apps are ported, or inspired by "jobs" used in the old
    ruby version of Hadashboard.
    
    They do not interact with Home Assistant at all, instead they fetch data
    from external sources and publish them as Appdaemon events. These events
    are are then consumed in dashboard widgets. Because we fetch and
    tranform data in the apps, the widgets themselves will be pretty simple,
    and simply format data, as it would any other Home Assistant event.
    
    Apps:
     - bysykkel: Status of Oslo City Bikes
     - ical: Events from iCal feed. Perferct for Google Calendar .ics-feed
     - ruter: Oslo public transport real time data
     - yr: Detailed weather reports from Yr (more fine grained forecast than
       Home Assistant currenly have)
     - eventCache: Caching of events, to make dashboards behave better
       before data is loaded
     - eventMonitor: Debugging of events

---
+++ conf/example_apps/eventCache.py
@@ -0,0 +1,75 @@
+import appdaemon.appapi as appapi
+import os
+import json
+
+"""
+
+Enable caching of appdaemon events.
+
+You would probably NOT want to use this for HomeAssistant events.
+
+The reason you would use this is probably because you're using custom events in
+some other appdaemon app, and want this event to be available (typically for
+hadashboard) before the app has been able to publish a fresh event.
+
+Simply put, you list up events you want to monitor, and every time this event
+is published, it is persisted to disk. When this app is restarted (probably
+because of a restart of appdaemon), it will publish all monitored events at
+startup.
+
+Arguments:
+ - cache: Location, on disk, where the events are stored
+ - evnets: List of events to monitor, and later publish
+
+"""
+class Cache(appapi.AppDaemon):
+    def initialize(self):
+        self.cache = self.args['cache']
+        self.events = self.args['events']
+
+        self.state = self.loadCache()
+        self.deprecateOldEvents()
+
+        self.publishCache()
+
+        for event in self.events:
+            self.log('watching event "{}" for state changes'.format(event))
+            self.listen_state(self.changed, event)
+
+    def loadCache(self):
+        if not os.path.exists(self.cache):
+            with open(self.cache, mode="w") as f:
+                json.dump({}, f)
+
+        with open(self.cache) as f:
+            try:
+                return json.load(f)
+            except:
+                return {}
+
+    def saveCache(self):
+        try:
+            with open(self.cache, mode="w") as f:
+                json.dump(self.state, f)
+        except:
+            self.log('oops during save of cache')
+
+
+    def deprecateOldEvents(self):
+        deprecatedEvents = [e for e in self.state if e not in self.events]
+        for deprecated in deprecatedEvents:
+            del self.state[deprecated]
+
+    def publishCache(self):
+        for event in self.events:
+            if event in self.state:
+                self.set_app_state(event, self.state[event])
+                self.log('published event: ' + event)
+            else:
+                self.log('event not in cache: ' + event)
+
+    def changed(self, entity, attribute, old, new, kwargs):
+        value = self.get_state(entity, 'all')
+        self.state[entity] = value
+        self.saveCache()
+
