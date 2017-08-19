c27f86e63083f4fecd6238b0d58c5d9b7e3d5ce6
Torkild Retvedt <torkild@retvedt.no>
2017-08-19 12:18:33 +0200

    Add example apps for bysykkel, yr, ical, ruter, eventCache and eventMonitor
    These apps are all a bit more "Hadashboard" centric than the other
    examples. These apps are ported, or inspired by "jobs" used in the old
    ruby version of Hadashboard.
    
    They do not interact with Home Assistant at all, instead they fetch data
    from external sources and publish them as Appdaemon events. These events
    are are then consumed in dashboard widgets. Because we fetch and
    tranform data in the apps, the widgets themselves will be pretty simple,
    and simply format data, as it would any other Home Assistant event.
    
    Apps:
     - bysykkel: Status of Oslo City Bikes
     - ical: Events from iCal feed. Perferct for Google Calendar .ics-feed
     - ruter: Oslo public transport real time data
     - yr: Detailed weather reports from Yr (more fine grained forecast than
       Home Assistant currenly have)
     - eventCache: Caching of events, to make dashboards behave better
       before data is loaded
     - eventMonitor: Debugging of events

---
+++ conf/example_apps/yr.py
@@ -0,0 +1,63 @@
+import appdaemon.appapi as appapi
+import requests
+import xml.etree.ElementTree as ET
+from datetime import datetime, timedelta
+
+"""
+
+Get detailed Yr weather data
+
+Arguments:
+ - event: Entity name when publishing event
+ - interval: Update interval, in minutes. Must be at least 10
+ - source: Yr xml source
+ - hours: Number of hours to forecast, at most 48
+
+"""
+
+disclaimer = "Weather forecast from Yr, delivered by the Norwegian Meteorological Institute and NRK"
+user_agent = "HomeAssistant/Appdaemon Python/requests"
+
+class Yr(appapi.AppDaemon):
+    def initialize(self):
+        self.url = self.args['source']
+        self.entity = self.args['event']
+        self.hours = self.args['hours']
+
+        inOneMinute = datetime.now() + timedelta(minutes = 1)
+        interval = int(self.args['interval'])
+
+        if interval < 10:
+            raise Exception('Update interval ({}) must be at least 10 minutes'.format(interval))
+
+        # delay first launch with one minute, run every 'interval' minutes
+        self.run_every(self.updateState, inOneMinute, interval * 60)
+
+    def updateState(self, kwargs):
+        forecast = self.fetchForecast()
+        self.set_app_state(self.entity, {
+            'state': "",
+            'attributes': forecast
+        })
+
+    def fetchData(self):
+        res = requests.get(self.url, headers={ 'User-Agent': user_agent })
+        return res.text
+
+    def fetchForecast(self):
+        data = self.fetchData()
+        root = ET.fromstring(data)
+        periods = root.find('.//tabular')
+        return {
+            'disclaimer': disclaimer,
+            'forecast': [{
+                'from': x.get('from'),
+                'to': x.get('to'),
+                'weather': x.find('symbol').get('name'),
+                'symbol': x.find('symbol').get('var'),
+                'precip': x.find('precipitation').get('value'),
+                'windSpeed': x.find('windSpeed').get('mps'),
+                'windDirection': x.find('windDirection').get('deg'),
+                'temp': x.find('temperature').get('value')
+            } for x in periods[:self.hours]],
+        }
