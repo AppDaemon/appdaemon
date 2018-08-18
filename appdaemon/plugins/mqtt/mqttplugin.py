d9103e2da7ae6b06685038aeb5a90b789fed5d73
Odianosen Ejale <mariatony.ejale@gmail.com>
2018-08-18 22:30:32 +0100

    Update mqttplugin.py

--- appdaemon/plugins/mqtt/mqttplugin.py
+++ appdaemon/plugins/mqtt/mqttplugin.py
@@ -59,7 +59,7 @@ class MqttPlugin:
     def stop(self):
         self.stopping = True
         if self.initialized:
-            self.log("{}: Stoping MQTT Plugin and Unsubcribing from URL {}:{}".format(self.name, self.mqtt_client_host, self.mqtt_client_port))
+            self.AD.log("INFO", "{}: Stoping MQTT Plugin and Unsubcribing from URL {}:{}".format(self.name, self.mqtt_client_host, self.mqtt_client_port))
             for topic in self.mqtt_client_topics:
                 self.log("{}: Unsubscribing from Topic: {}".format(self.name, topic))
                 result = self.mqtt_client.unsubscribe(topic)
