cc0fd57dc646c0ce4855a5e659225d2bbb2514ce
Andrew Cockburn <andrew@acockburn.com>
2018-11-26 16:18:31 -0500

    Fix callbacks locking issue

--- appdaemon/callbacks.py
+++ appdaemon/callbacks.py
@@ -32,31 +32,32 @@ class Callbacks:
 
     def get_callback_entries(self, type="all"):
         callbacks = {}
-        for name in self.callbacks.keys():
-            for uuid_ in self.callbacks[name]:
-                if self.callbacks[name][uuid_]["type"] == type or type == "all":
-                    if name not in callbacks:
-                        callbacks[name] = {}
-                    callbacks[name][str(uuid_)] = {}
-                    if "entity" in self.callbacks[name][uuid_]:
-                        if self.callbacks[name][uuid_]["entity"] is None:
-                            callbacks[name][str(uuid_)]["entity"] = "None"
+        with self.callbacks_lock:
+            for name in self.callbacks.keys():
+                for uuid_ in self.callbacks[name]:
+                    if self.callbacks[name][uuid_]["type"] == type or type == "all":
+                        if name not in callbacks:
+                            callbacks[name] = {}
+                        callbacks[name][str(uuid_)] = {}
+                        if "entity" in self.callbacks[name][uuid_]:
+                            if self.callbacks[name][uuid_]["entity"] is None:
+                                callbacks[name][str(uuid_)]["entity"] = "None"
+                            else:
+                                callbacks[name][str(uuid_)]["entity"] = self.callbacks[name][uuid_]["entity"]
                         else:
-                            callbacks[name][str(uuid_)]["entity"] = self.callbacks[name][uuid_]["entity"]
-                    else:
-                        callbacks[name][str(uuid_)]["entity"] = "None"
-                    if "event" in self.callbacks[name][uuid_]:
-                        callbacks[name][str(uuid_)]["event"] = self.callbacks[name][uuid_]["event"]
-                    else:
-                        callbacks[name][str(uuid_)]["event"] = "None"
-                    callbacks[name][str(uuid_)]["type"] = self.callbacks[name][uuid_]["type"]
-                    callbacks[name][str(uuid_)]["kwargs"] = ""
-                    callbacks[name][str(uuid_)]["kwargs"] = utils.get_kwargs(self.callbacks[name][uuid_]["kwargs"])
+                            callbacks[name][str(uuid_)]["entity"] = "None"
+                        if "event" in self.callbacks[name][uuid_]:
+                            callbacks[name][str(uuid_)]["event"] = self.callbacks[name][uuid_]["event"]
+                        else:
+                            callbacks[name][str(uuid_)]["event"] = "None"
+                        callbacks[name][str(uuid_)]["type"] = self.callbacks[name][uuid_]["type"]
+                        callbacks[name][str(uuid_)]["kwargs"] = ""
+                        callbacks[name][str(uuid_)]["kwargs"] = utils.get_kwargs(self.callbacks[name][uuid_]["kwargs"])
 
-                    callbacks[name][str(uuid_)]["function"] = self.callbacks[name][uuid_]["function"].__name__
-                    callbacks[name][str(uuid_)]["name"] = self.callbacks[name][uuid_]["name"]
-                    callbacks[name][str(uuid_)]["pin_app"] = "True" if self.callbacks[name][uuid_]["pin_app"] is True else "False"
-                    callbacks[name][str(uuid_)]["pin_thread"] = self.callbacks[name][uuid_]["pin_thread"] if self.callbacks[name][uuid_]["pin_thread"] != -1 else "None"
+                        callbacks[name][str(uuid_)]["function"] = self.callbacks[name][uuid_]["function"].__name__
+                        callbacks[name][str(uuid_)]["name"] = self.callbacks[name][uuid_]["name"]
+                        callbacks[name][str(uuid_)]["pin_app"] = "True" if self.callbacks[name][uuid_]["pin_app"] is True else "False"
+                        callbacks[name][str(uuid_)]["pin_thread"] = self.callbacks[name][uuid_]["pin_thread"] if self.callbacks[name][uuid_]["pin_thread"] != -1 else "None"
         return callbacks
 
     def clear_callbacks(self, name):
