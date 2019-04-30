e7462541083be8a2fb74832630ae0f5ab28fb6a2
Odianosen Ejale <mariatony.ejale@gmail.com>
2019-04-30 16:59:49 +0100

    Update utility_loop.py

--- appdaemon/utility_loop.py
+++ appdaemon/utility_loop.py
@@ -170,10 +170,3 @@ class Utility:
 
             if self.AD.app_management is not None:
                 await self.AD.app_management.terminate()
-
-    async def set_production_mode(self, mode):
-        if mode:
-            self.logger.info("AD Production Mode Activated")
-        else:
-            self.logger.info("AD Production Mode Deactivated")
-        self.AD.production_mode = mode
