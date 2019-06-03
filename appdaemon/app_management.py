8732917e13fc47b6e5cc4232554434f7926b9c38
Odianosen Ejale <mariatony.ejale@gmail.com>
2019-06-03 12:30:21 +0100

    Removed initial Sensor update

--- appdaemon/app_management.py
+++ appdaemon/app_management.py
@@ -452,8 +452,8 @@ class AppManagement:
                     self.logger.info("Found %s active apps", active_apps)
                     self.logger.info("Found %s inactive apps", inactive_apps)
 
-                await self.set_state(self.active_apps_sensor, state = active_apps)
-                await self.set_state(self.inactive_apps_sensor, state = inactive_apps)
+                #await self.set_state(self.active_apps_sensor, state = active_apps)
+                #await self.set_state(self.inactive_apps_sensor, state = inactive_apps)
 
             # Now we know if we have any new apps we can create new threads if pinning
 
