2cadbea81bf4dc242b34a9fb0e09da7e11b398ec
Odianosen Ejale <mariatony.ejale@gmail.com>
2019-07-04 12:56:07 +0100

    Update threading.py

--- appdaemon/threading.py
+++ appdaemon/threading.py
@@ -440,6 +440,20 @@ class Threading:
 
         return unconstrained
 
+    async def check_days_constraint(self, args, name):
+        unconstrained = True
+        if "constrain_days" in args:
+            days = args["constrain_days"]
+            now = await self.AD.sched.get_now()
+            daylist = []
+            for day in days.split(","):
+                daylist.append(await utils.run_in_executor(self, utils.day_of_week, day))
+
+            if now.weekday() not in daylist:
+                unconstrained = False
+
+        return unconstrained
+
     #
     # Workers
     #
@@ -529,6 +543,9 @@ class Threading:
                 unconstrained = False
         if not await self.check_time_constraint(self.AD.app_management.app_config[name], name):
             unconstrained = False
+        elif not await self.check_days_constraint(self.AD.app_management.app_config[name], name):
+            unconstrained = False
+
         #
         # Callback level constraints
         #
@@ -539,6 +556,8 @@ class Threading:
                 if not constrained:
                     unconstrained = False
             if not await self.check_time_constraint(myargs["kwargs"], name):
+                unconstrained = False
+            elif not await self.check_days_constraint(self.AD.app_management.app_config[name], name):
                 unconstrained = False
 
         if unconstrained:
@@ -635,4 +654,3 @@ class Threading:
             self.logger.error("Unknown callback type: %s", type)
 
         return False
-
