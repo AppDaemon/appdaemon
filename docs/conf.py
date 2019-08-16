e2d278c9210fa70b568809770d2fc1a555e20c67
acockburn <andrew@acockburn.com>
2019-08-16 09:24:19 -0400

    Fix docs build

--- docs/conf.py
+++ docs/conf.py
@@ -37,7 +37,7 @@ sys.path.insert(0, os.path.abspath('..'))
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx.ext.autodoc', 'sphinxcontrib.napoleon'
+    'sphinx.ext.autodoc', 'sphinx.ext.napoleon'
 ]
 
 
