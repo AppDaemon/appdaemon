b4e03efe730d0b85d474c29c84b3eb90c3ba358b
apz <antpza@gmail.com>
2022-06-28 22:46:43 +0200

    rename HA_CERT_VERIFY

--- dockerStart.sh
+++ dockerStart.sh
@@ -33,9 +33,9 @@ if [ -n "$TOKEN" ]; then
   sed -i "s/^      token:.*/      token: $(echo $TOKEN | sed -e 's/\\/\\\\/g; s/\//\\\//g; s/&/\\\&/g')/" $CONF/appdaemon.yaml
 fi
 
-# if ENV CERT_VERIFY is set, change the value in appdaemon.yaml
-if [ -n "$CERT_VERIFY" ]; then
-  sed -i "s/^      cert_verify:.*/      cert_verify: $(echo $CERT_VERIFY | sed -e 's/\\/\\\\/g; s/\//\\\//g; s/&/\\\&/g')/" $CONF/appdaemon.yaml
+# if ENV HA_CERT_VERIFY is set, change the value in appdaemon.yaml
+if [ -n "$HA_CERT_VERIFY" ]; then
+  sed -i "s/^      cert_verify:.*/      cert_verify: $(echo $HA_CERT_VERIFY | sed -e 's/\\/\\\\/g; s/\//\\\//g; s/&/\\\&/g')/" $CONF/appdaemon.yaml
 fi
 
 # if ENV DASH_URL is set, change the value in appdaemon.yaml
