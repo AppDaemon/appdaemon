d7f9ca7d9a9eaa59976c0442eb9c71a4c63d363b
acockburn <andrew@acockburn.com>
2023-03-07 09:42:54 -0500

    Fix typo

--- appdaemon/appdaemon.py
+++ appdaemon/appdaemon.py
@@ -155,7 +155,7 @@ class AppDaemon:
         utils.process_arg(self, "internal_function_timeout", kwargs, int=True)
 
         self.use_dictionary_unpacking = False
-        utils.process_arg(self, "use_dicitonary_unpacking", kwargs)
+        utils.process_arg(self, "use_dictionary_unpacking", kwargs)
 
         self.namespaces = {}
         utils.process_arg(self, "namespaces", kwargs)
