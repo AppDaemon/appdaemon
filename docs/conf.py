c89a30efa17df3416d63a35da9671ba7bb3e23a3
acockburn <andrew@acockburn.com>
2023-03-24 09:08:45 -0400

    Fix AD path

--- docs/conf.py
+++ docs/conf.py
@@ -7,6 +7,13 @@
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 from appdaemon.version import __version__
+import sys
+import os
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+sys.path.insert(0, os.path.abspath(".."))
 
 project = "AppDaemon"
 copyright = "2016-2023, Andrew Cockburn"
