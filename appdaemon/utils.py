f11a6adcf02011107ebada99add955d1c16b7790
John Lancaster <32917998+jsl12@users.noreply.github.com>
2025-04-27 14:38:12 -0500

    docstrings for utils

--- appdaemon/utils.py
+++ appdaemon/utils.py
@@ -204,34 +204,41 @@ def check_state(logger, new_state, callback_state, name) -> bool:
 
     return passed
 
+
 P = ParamSpec("P")
 R = TypeVar("R")
 
+
 def sync_decorator(coro_func: Callable[P, Awaitable[R]]) -> Callable[P, R]:
+    """Wrap a coroutine function to ensure it gets run in the main thread.
+    
+    This allows users to run async ADAPI methods as if they were regular sync methods. It works by checking to see if
+    the function is being run in the main thread, which has the async event loop in it. If it is the main loop, then it
+    creates a task and returns it. If it isn't, then it runs the coroutine in the main thread using
+    ``run_coroutine_threadsafe``.
+
+    See
+    `scheduling from other threads <https://docs.python.org/3/library/asyncio-task.html#scheduling-from-other-threads>`__
+    for more details.
+    """
     @wraps(coro_func)
     def wrapper(self, *args, timeout: str | int | float | timedelta | None = None, **kwargs) -> R:
-        # self.logger.debug(f"Wrapping async function {coro_func.__qualname__}")
         ad: "AppDaemon" = self.AD
 
-        try:
-            # Checks to see if it's being called from the main thread, which has the event loop in it
-            in_main_thread = ad.main_thread_id == threading.current_thread().ident
+        # Checks to see if it's being called from the main thread, which has the event loop in it
+        in_main_thread = ad.main_thread_id == threading.current_thread().ident
 
-            if 'timeout' in inspect.signature(coro_func).parameters:
-                kwargs['timeout'] = timeout
+        # pass through the timeout argument if the function accepts it
+        if 'timeout' in inspect.signature(coro_func).parameters:
+            kwargs['timeout'] = timeout
 
-            coro = coro_func(self, *args, **kwargs)
-            if in_main_thread:
-                task = asyncio.create_task(coro)
-                ad.futures.add_future(self.name, task)
-                return task
-            else:
-                return run_coroutine_threadsafe(self, coro, timeout=timeout)
-        except Exception:
-            # except Exception as e:
-            # ad.threading.logger.error(f"Error running coroutine threadsafe: {e}")
-            # ad.threading.logger.error(format_exception(e))
-            raise
+        coro = coro_func(self, *args, **kwargs)
+        if in_main_thread:
+            task = asyncio.create_task(coro)
+            ad.futures.add_future(self.name, task)
+            return task
+        else:
+            return run_coroutine_threadsafe(self, coro, timeout=timeout)
 
     return wrapper
 
@@ -276,13 +283,43 @@ def format_seconds(secs):
 
 
 def convert_timedelta(s: str | int | float | timedelta | None) -> timedelta:
+    """Convert disparate types into a timedelta object.
+    
+    Args:
+        s (str | int | float | timedelta | None): The value to convert. Can be a string, int, float, or timedelta.
+            Numbers get interpreted as seconds. Strings can in different formats either ``HH:MM:SS``, ``MM:SS``, or
+            ``SS``.
+
+    Returns:
+        Timedelta object.
+
+    Examples:
+        >>> convert_timedelta(0.025374)
+        datetime.timedelta(microseconds=25374)
+
+        >>> convert_timedelta(0.687)
+        datetime.timedelta(microseconds=687000)
+
+        >>> convert_timedelta(2.5)
+        datetime.timedelta(seconds=2, microseconds=500000)
+
+        >>> convert_timedelta("25")
+        datetime.timedelta(seconds=25)
+
+        >>> convert_timedelta("02:30")
+        datetime.timedelta(seconds=150)
+
+        >>> convert_timedelta("00:00:00")
+        datetime.timedelta(0)
+
+    """
     match s:
         case timedelta():
             return s
         case int() | float():
             return timedelta(seconds=s)
         case str():
-            parts = tuple(float(p) for p in s.split(":"))
+            parts = tuple(float(p.strip()) for p in s.split(":"))
             match len(parts):
                 case 1:
                     return timedelta(seconds=parts[0])
@@ -301,18 +338,54 @@ def convert_timedelta(s: str | int | float | timedelta | None) -> timedelta:
             raise ValueError(f"Invalid type for timedelta: {type(s)}. Must be str, int, float, or timedelta")
 
 
-def format_timedelta(td: timedelta) -> str:
-    seconds = td.total_seconds()
-    if seconds == 0:
-        return "No time"
-    elif seconds < 0.1:
-        return f"{seconds * 10**3:.3f}ms"
-    elif seconds < 1:
-        return f"{seconds * 10**3:.0f}ms"
-    elif seconds < 10:
-        return f"{td.total_seconds():.1f}s"
-    else:
-        return str(td).split(".")[0]
+def format_timedelta(td: str | int | float | timedelta | None) -> str:
+    """Format a timedelta object into a human-readable string.
+    
+    There are different brackets for lengths of time that will format the strings differently.
+
+    Uses ``convert_timedelta`` to convert the input into a timedelta object before formatting the string.
+
+    Examples:
+        >>> format_timedelta(0.025374)
+        '25.374ms'
+
+        >>> format_timedelta(0.687)
+        '687ms'
+
+        >>> format_timedelta(2.5)
+        '2.5s'
+
+        >>> format_timedelta(25)
+        '25s'
+
+        >>> format_timedelta(None)
+        'never'
+
+        >>> format_timedelta(0)
+        'No time'
+    
+    """
+    match td:
+        case None:
+            return 'never'
+        case _:
+            td = convert_timedelta(td)
+            seconds = td.total_seconds()
+            if seconds == 0:
+                return "No time"
+            elif seconds < 0.1:
+                return f"{seconds * 10**3:.3f}ms"
+            elif seconds < 1:
+                return f"{seconds * 10**3:.0f}ms"
+            elif seconds < 10:
+                return f"{seconds:.1f}s"
+            else:
+                td = timedelta(seconds=round(seconds, 0)) # Round off the seconds for longer durations
+                res = str(td)
+                hours = int(seconds / 3600)
+                if hours == 0: # Remove the hours portion if it's 0
+                    res = res.split(':', 1)[1]
+                return res
 
 
 def deep_compare(check: dict, data: dict) -> bool:
@@ -400,6 +473,10 @@ def warning_decorator(
     Only works on methods of AppDaemon subsystems because it uses the attributes:
         - self.logger
         - self.AD
+
+    Raises:
+        By default, only ever re-raises an AppDaemonException
+
     """
 
     def decorator(func):
@@ -472,15 +549,22 @@ async def run_in_executor(self, fn, *args, **kwargs) -> Any:
     return await future
 
 
-def run_coroutine_threadsafe(self: "ADBase", coro: Coroutine, timeout: str | int | float | timedelta | None = None) -> Any:
-    """This runs an instantiated coroutine (async) from sync code. This handles the logic for cancelling
-    coroutines that run too long.
+def run_coroutine_threadsafe(
+    self: "ADBase",
+    coro: Coroutine[Any, Any, R],
+    timeout: str | int | float | timedelta | None = None
+) -> R:
+    """Run an instantiated coroutine (async) from sync code.
+    
+    This wraps the native python function ``asyncio.run_coroutine_threadsafe`` with logic to add a timeout. See
+    `scheduling from other threads <https://docs.python.org/3/library/asyncio-task.html#scheduling-from-other-threads>`__
+    for more details.
 
     Args:
-        self (ADBase): Needs to have a ``self.AD`` attribute with the ``AppDaemon`` object.
+        self (ADBase): Needs to have a ``self.AD`` attribute with a reference to the ``AppDaemon`` object.
         coro (Coroutine): An instantiated coroutine that hasn't been awaited.
-        timeout (float | None, optional): Optional timeout. Defaults to None,
-            which will block indefinitely
+        timeout (float | None, optional): Optional timeout to use. If no value is provided then the value set in
+            ``appdaemon.internal_function_timeout`` in the ``appdaemon.yaml`` file will be used.
 
     Returns:
         Result from the coroutine
@@ -490,6 +574,7 @@ def run_coroutine_threadsafe(self: "ADBase", coro: Coroutine, timeout: str | int
 
     if self.AD.loop.is_running():
         future = asyncio.run_coroutine_threadsafe(coro, self.AD.loop)
+        self.AD.futures.add_future(self.name, future)
         try:
             return future.result(timeout.total_seconds())
         except concurrent.futures.CancelledError:
