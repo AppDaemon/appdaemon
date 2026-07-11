b8b06ba7e39b19674c49c2e0d196f10f422a107c
Andrew Cockburn <andrew@acockburn.com>
2026-07-11 07:50:38 -0400

    bugfix

--- tests/unit/test_misc.py
+++ tests/unit/test_misc.py
@@ -2,27 +2,27 @@ import pytest
 from appdaemon.utils.misc import deep_compare
 
 pytestmark = [
-	pytest.mark.ci,
-	pytest.mark.unit,
+    pytest.mark.ci,
+    pytest.mark.unit,
 ]
 
 
 def test_deep_compare_missing_nested_key_returns_false() -> None:
-	check = {"guest": {"sleepguest": True}}
-	data = {"guest": {}}
+    check = {"guest": {"sleepguest": True}}
+    data = {"guest": {}}
 
-	assert deep_compare(check, data) is False
+    assert deep_compare(check, data) is False
 
 
 def test_deep_compare_equal_nested_dict_returns_true() -> None:
-	check = {"guest": {"sleepguest": True}}
-	data = {"guest": {"sleepguest": True, "other": "value"}}
+    check = {"guest": {"sleepguest": True}}
+    data = {"guest": {"sleepguest": True, "other": "value"}}
 
-	assert deep_compare(check, data) is True
+    assert deep_compare(check, data) is True
 
 
 def test_deep_compare_missing_top_level_key_returns_false() -> None:
-	check = {"sleepguest": True}
-	data = {}
+    check = {"sleepguest": True}
+    data = {}
 
-	assert deep_compare(check, data) is False
+    assert deep_compare(check, data) is False
